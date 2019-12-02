923bb2d963a4a5e576cdbf6e1428d830e84d83f0
Dmitry Yaskovich <kadavr95@outlook.com>
2019-12-02 20:25:43 +0300

    fixes

--- javascript.js
+++ javascript.js
@@ -1,20 +1,15 @@
 window.onload = function() 
 {
-    let lab=[];
-    let dat=[];
     let data_set=[];
     let coef=[6,0,-4,-3,5,6,-6,-13,7,44,64,44,7,-13,-6,6,5,-3,-4,0,6]
     let sum1=0, sum2=0;
     for (i=22; i>2; i=i-0.01){
-        lab.push(2/i);
         sum1=Math.cos(2*3.14/i)*coef[0];
         sum2=Math.sin(2*3.14/i)*coef[0];
         for (j=2; j<21; j++){
             sum1+=Math.cos(2*3.14/i*j)*coef[j];
             sum2+=Math.sin(2*3.14/i*j)*coef[j];
-            
         }
-        dat.push(20*Math.log10(Math.sqrt(sum1*sum1+sum2*sum2)));
         data_set.push({x: 2/i, y: 20*Math.log10(Math.sqrt(sum1*sum1+sum2*sum2))})
     }
 	if (document.getElementById("result"))
